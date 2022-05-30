d081d01bfd99deb903dd76e6848a05e77864474d
imustend <plochockitymon@gmail.com>
2022-05-30 14:57:55 +0200

    added user input

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,27 @@ pub mod network_ip;
 pub mod number_of_hosts;
 pub mod broadcast_ip;
 
+use std::io;
+
 fn main() {
-    let ip = ip::build_ip("172.16.0.0 ".to_string());
+	let mut ip_string = String::new();
+	let mut mask_string = String::new();
+
+	println!("ip?");
+    io::stdin()
+        .read_line(&mut ip_string)
+        .expect("Failed to read line");
+
+	println!("mask?");
+    io::stdin()
+        .read_line(&mut mask_string)
+        .expect("Failed to read line");
+
+    let ip = ip::build_ip(ip_string);
 	show::print_ip("ip".to_string(), &ip);
 	println!("ip class: {}", ip_class::ip_class(&ip));
 
-	let mask = mask::build_mask("255.224.0.0".to_string());
+	let mask = mask::build_mask(mask_string);
 	show::print_musk("mask".to_string(), &mask);
 	println!("number of hosts: {}", number_of_hosts::get_number_of_hosts(&mask));
 
